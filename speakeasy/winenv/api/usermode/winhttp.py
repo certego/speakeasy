dc9c48b9db68995c7ef54afa161ebba2b7f0ffb9
0ssigeno <simone.berni2@studio.unibo.it>
2021-10-12 17:46:37 +0200

    Fix https

--- speakeasy/winenv/api/usermode/winhttp.py
+++ speakeasy/winenv/api/usermode/winhttp.py
@@ -222,7 +222,7 @@ class WinHttp(api.ApiHandler):
             port,
             headers=req_str,
             data=body,
-            proto="https" if not req.is_secure() else "http",
+            proto="https" if req.is_secure() else "http",
             method=req.verb.upper(),
         )
         return rv
